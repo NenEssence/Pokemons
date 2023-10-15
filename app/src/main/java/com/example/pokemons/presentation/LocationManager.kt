9f276eb9eb77025539ec57a32045c6113893258b
Andrei Sheverinov <jobboxsh@mail.ru>
2023-10-15 16:29:12 +0300

    Result Api added

--- app/src/main/java/com/example/pokemons/presentation/LocationManager.kt
+++ app/src/main/java/com/example/pokemons/presentation/LocationManager.kt
@@ -1,7 +1,5 @@
 package com.example.pokemons.presentation
 
-import android.Manifest
-import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
@@ -9,20 +7,19 @@ import android.location.LocationListener
 import android.location.LocationManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class LocationManager(private val context: Context):LocationListener, AppCompatActivity() {
 
-    private val locationPermissionCode = 2
+
     private val locationManager: LocationManager =
         context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
+
     fun getLocation() {
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(context as Activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
-        }
+        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5f, this)
+        }
     }
 
     override fun onLocationChanged(location: Location) {
--- app/src/main/java/com/example/pokemons/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/pokemons/presentation/main/MainActivity.kt
@@ -1,18 +1,58 @@
 package com.example.pokemons.presentation.main
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.util.Log
+import android.widget.Toast
 import com.example.pokemons.presentation.rv.PokemonAdapter
 import com.example.pokemons.databinding.ActivityMainBinding
-import com.example.pokemons.presentation.LocationManager
 import com.example.pokemons.presentation.details.PokemonDetailsActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.pokemons.presentation.LocationManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity () {
     private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissions =
+         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ permissions ->
+             permissions.entries.forEach {
+                 Log.e("DEBUG", "${it.key} = ${it.value}")
+             }
+
+             if(permissions.containsValue(false)){
+                 Toast.makeText(this,"Доступ к геолокации запрещен",Toast.LENGTH_SHORT).show()
+             }
+             else {
+                 Toast.makeText(this,"Доступ к геолокации разрешен",Toast.LENGTH_SHORT).show()
+                 val locationManager = LocationManager(this)
+                 locationManager.getLocation()
+             }
+             }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val locationManager = LocationManager(this)
+        when {
+            ((this.checkSelfPermission(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED) and (this.checkSelfPermission(
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED))-> {
+                locationManager.getLocation()
+            }
+            else -> {
+                requestPermissions.launch(
+                    arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    )
+            }
+        }
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -22,8 +62,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.rvList.adapter = adapter
 
-        val locationManager = LocationManager(this)
-        locationManager.getLocation()
+
     }
 
 
