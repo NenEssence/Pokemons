afa87731dfa572c7309b6ef70b3c6437a07d6679
Andrei Sheverinov <jobboxsh@mail.ru>
2023-10-25 10:21:47 +0300

    Result Api fix 1

--- app/src/main/java/com/example/pokemons/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/pokemons/presentation/main/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.pokemons.presentation.main
 
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
-import android.util.Log
 import android.widget.Toast
 import com.example.pokemons.presentation.rv.PokemonAdapter
 import com.example.pokemons.databinding.ActivityMainBinding
@@ -16,19 +14,13 @@ import com.example.pokemons.presentation.LocationManager
 class MainActivity : AppCompatActivity () {
     private lateinit var binding: ActivityMainBinding
 
-    private val requestPermissions =
+     private val requestPermissions =
          registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ permissions ->
-             permissions.entries.forEach {
-                 Log.e("DEBUG", "${it.key} = ${it.value}")
-             }
-
              if(permissions.containsValue(false)){
                  Toast.makeText(this,"Доступ к геолокации запрещен",Toast.LENGTH_SHORT).show()
              }
              else {
                  Toast.makeText(this,"Доступ к геолокации разрешен",Toast.LENGTH_SHORT).show()
-                 val locationManager = LocationManager(this)
-                 locationManager.getLocation()
              }
              }
 
@@ -36,23 +28,7 @@ class MainActivity : AppCompatActivity () {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val locationManager = LocationManager(this)
-        when {
-            ((this.checkSelfPermission(
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED) and (this.checkSelfPermission(
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED))-> {
-                locationManager.getLocation()
-            }
-            else -> {
-                requestPermissions.launch(
-                    arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                        android.Manifest.permission.ACCESS_FINE_LOCATION)
-                    )
-            }
-        }
-
+        LocationManager(this).getLocation(requestPermissions)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -61,10 +37,7 @@ class MainActivity : AppCompatActivity () {
         adapter.onClick = { openDetailActivity(it)}
 
         binding.rvList.adapter = adapter
-
-
     }
-
 
     private fun openDetailActivity(pokemon: Parcelable){
         val intent = Intent(this, PokemonDetailsActivity::class.java)
